caefe3d13c93667afbe212264f16ad4176f54071
Kevin Cunningham <kevin@kevincunningham.co.uk>
2026-01-26 14:49:53 +0000

    adds random

--- actual-code/03-flow-control/06_for.py
+++ actual-code/03-flow-control/06_for.py
@@ -4,10 +4,10 @@
 target = int(input("What number do you want? "))
 
 for value in range(10):
-  print(f"Checking {value} to see if it matches {target}")
   if value == 3:
     print("I don't think I'll check this one")
     continue  # ignores the rest of the current run, and moves on to the next value
+  print(f"Checking {value} to see if it matches {target}")
   if target == value:
     print("Found it!")
     break  # escape the parent loop early!
---
+++ actual-code/03-flow-control/07_random.py
@@ -0,0 +1,4 @@
+import random
+secret = random.randint(1,50)
+
+print(secret)
